831818a00a5e8302b196b0c2f4ba9ea0b7236e9e
azemoh <buzzme46@gmail.com>
2015-08-28 14:53:32 +0100

    Add Fontawesome CDN

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -21,7 +21,6 @@ namespace WebApp {
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                    "~/Content/Stylesheets/lib/font-awesome.min.css",
                     "~/Content/Stylesheets/site.css"));
         }
     }
